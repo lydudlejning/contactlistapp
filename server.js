e2e34b179dcb9696d8db16fc8a22e3d02bcdaeee
lydudlejning <adm@lydudlejning.net>
2015-09-09 15:14:09 +0200

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/contactlist', function (req, res) {
 });
 
 app.post('/contactlist', function (req, res) {
-	console.log("POST:", req.body)
+	console.log(req.body)
 	db.contactlist.insert(req.body, function(err, doc) {
 		res.json(doc);
 	});
@@ -41,7 +41,7 @@ app.get('/contactlist/:id', function (req, res) {
 
 app.put('/contactlist/:id', function (req, res) {
   	var id = req.params.id;
-  	console.log("PUT <br>" + req.body.name);
+  	console.log(req.body.name);
   	db.contactlist.findAndModify({
     	query: {_id: mongojs.ObjectId(id)},
     	update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
