22387159720ba6e105f93c4f677099123fa522fc
lydudlejning <adm@lydudlejning.net>
2015-09-09 14:53:20 +0200

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/contactlist', function (req, res) {
 });
 
 app.post('/contactlist', function (req, res) {
-	console.log("POST<br>" + req.body)
+	console.log("POST:" + req.body)
 	db.contactlist.insert(req.body, function(err, doc) {
 		res.json(doc);
 	});
