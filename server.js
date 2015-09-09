f46c3bc5d015ab824ee6b898245dd4c79250ea7e
lydudlejning <adm@lydudlejning.net>
2015-09-09 14:16:12 +0200

    My 10th commit

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.put('/contactlist/:id', function (req, res) {
   );
 });
 
-app.listen(process.env.PORT);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000);
+console.log("Server running");
